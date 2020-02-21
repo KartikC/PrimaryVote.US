9b998b08024024ee8e41ecc92ba96657c13fa908
kartiksathappan <kartiksathappan@gmail.com>
2020-02-20 23:30:42 -0800

    getting started with svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from "svelte";
+
+  	let stateData = [];
+
+	onMount(async () => {
+		const res = await fetch(`state-data.json`);
+		stateData = await res.json();
+		console.log(stateData);
+	});
+	
+</script>
+
+{#each stateData as state}
+	<h3>{state.name}</h3>
+	vote in person by {state.dates.person}<br>
+	vote online by {state.dates.online}<br>
+	vote via mail by {state.dates.mail}<br>
+	<br>
+{/each}
