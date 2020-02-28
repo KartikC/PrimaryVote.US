e70ebb41f6d9844728e792dc3aa7b33dd22ebe7e
kartiksathappan <kartiksathappan@gmail.com>
2020-02-28 15:08:28 -0800

    better error message

--- src/TooWide.svelte
+++ src/TooWide.svelte
@@ -14,11 +14,11 @@
         font-weight: bold;
         color: #E8F1F2;
         text-align: left;
-        font-size: 2.5em;
+        font-size: 2em;
         box-sizing: border-box;
     }
 </style>
 
 <span class="text" in:fly="{{ y: 200, duration: 1000 }}" out:fly="{{ y: -200, duration: 1000 }}">
-    YOUR WINDOW IS TOO WIDE &#x1F605
+    YOUR BROWSER WINDOW IS TOO WIDE<br/>MAKE IT SMALLER TO CONTINUE &#x1F605
 </span>
